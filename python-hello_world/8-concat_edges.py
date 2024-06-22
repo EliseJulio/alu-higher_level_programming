26fd9f897e4159319cdf8c5b0ba50c288c71aa1e
EliseJulio <j.hakiziman1@alustudent.com>
2024-06-22 12:00:21 -0700

    Python Learning

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:67] + str[107:112] + str[:6])
-
