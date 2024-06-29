5f91751fa250e7ed2c9242b9f61b45de58bc5583
EliseJulio <j.hakiziman1@alustudent.com>
2024-06-29 12:46:11 -0700

    Python more structures

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    keys_to_delete = [key for key in a_dictionary if a_dictionary[key] == value]
+    keys_to_delete = [
+            key for key in a_dictionary if a_dictionary[key] == value
+            ]
     for key in keys_to_delete:
         del a_dictionary[key]
     return a_dictionary
 
 
 if __name__ == "__main__":
-    a_dictionary = {
-        'lang': "C",
-        'track': "Low",
-        'pref': "C",
-        'ids': [1, 2, 3]
-    }
+    a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
     new_dict = complex_delete(a_dictionary, 'C')
-    print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-    print_sorted_dictionary(a_dictionary)
-    print("--")
-    print_sorted_dictionary(new_dict)
-
-    print("--")
-    print("--")
+    print(new_dict)
     new_dict = complex_delete(a_dictionary, 'c_is_fun')
-    print_sorted_dictionary(a_dictionary)
-    print("--")
-    print_sorted_dictionary(new_dict)
+    print(new_dict)
