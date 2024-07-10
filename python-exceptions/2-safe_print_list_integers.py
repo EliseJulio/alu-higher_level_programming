11578b5aee755ee65053dbe5355af8ce8fe1b162
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-10 13:58:01 -0700

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    printed_nums = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
+            print('{:d}'.format(my_list[i]), end="")
+            printed_nums += 1
+        except (TypeError, ValueError):
             continue
-        except IndexError:
-            break
     print()
-    return count
-
-
-if __name__ == "__main__":
-    my_list = [1, "hello", 3, "world", 5]
-    result = safe_print_list_integers(my_list, 7)
-    print("Number of integers printed:", result)
+    return printed_nums
