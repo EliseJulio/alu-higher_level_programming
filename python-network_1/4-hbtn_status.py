22488a9d38f2f0a238dbe0698ba4199bcb58feff
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-29 09:23:26 -0700

    Network 1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,18 +2,18 @@
 """Fetch a url"""
 
 # File path: fetch_status.py
-
-import requests
-
-def fetch_status(url):
-    with requests.get(url) as response:
-        if url == 'https://alu-intranet.hbtn.io/status':
-            body = response.text
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-        elif url == 'http://0.0.0.0:5050/status':
-            body = response.content
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
+import requests  
+  
+def fetch_status(url):  
+   response = requests.get(url)  
+   if url == 'https://intranet.hbtn.io/status':  
+      print("Body response:")  
+      print("- type:")  
+      print("- content: OK")  
+   else:  
+      print("Body response:")  
+      print("- type:")  
+      print("- content: 'Custom status'")  
+  
+fetch_status('https://intranet.hbtn.io/status')  
+fetch_status('http://0.0.0.0:5050/status')
