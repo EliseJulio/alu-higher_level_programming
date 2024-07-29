e7d67322c83aad4575d2d384cddffe49041ead7f
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-29 06:25:57 -0700

    Python network 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 '''a Python script that fetches a url'''
 
+# File path: fetch_status.py
 
 import urllib.request
 
+url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Fetch a url"""
 
+# File path: fetch_status.py
+
 import requests
 
+url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+with requests.get(url) as response:
+    body = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
