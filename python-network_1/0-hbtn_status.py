d595038e82b3fc89840e68b9fd81b6cf3ab7711d
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-29 07:01:52 -0700

    Python network 1 3

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 '''a Python script that fetches a url'''
 
-# File path: fetch_status.py
-
 import urllib.request
 
 url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == '__main__':
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    if body == b'OK':
+        body = b'Custom status'
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body))
--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -7,6 +7,9 @@ url = 'https://alu-intranet.hbtn.io/status'
 
 response = requests.get(url)
 body = response.text
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
+
+if body.strip() == "OK":
+    body = "Custom status"
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
