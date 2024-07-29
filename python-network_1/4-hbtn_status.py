df321e38abe20b8cac43880f852d69ba81384c54
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-29 06:48:57 -0700

    Python network 1 2

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,11 +2,11 @@
 """Fetch a url"""
 
 import requests
+
 url = 'https://alu-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    response = requests.get(url)
-    body = response.text
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+response = requests.get(url)
+body = response.text
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
