a14542ae852d2f5cdd929fa105d363885d13778b
EliseJulio <j.hakiziman1@alustudent.com>
2024-07-29 09:52:31 -0700

    Python new networks 1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" module uses the request module
-    to send a request to server """
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
 
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    data = r.text
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
